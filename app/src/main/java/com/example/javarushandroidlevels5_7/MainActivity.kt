71f1c3e5723962874b4f3a82495590a9e4c699a0
Zhekthedark <Zhekfake@gmail.com>
2019-06-17 15:18:10 +0300

    сделал 5 лвл и 14 лекцию (добавил кнопки увеличения и уменьшения)

--- app/src/main/java/com/example/javarushandroidlevels5_7/MainActivity.kt
+++ app/src/main/java/com/example/javarushandroidlevels5_7/MainActivity.kt
@@ -5,25 +5,40 @@ import android.os.Bundle
 
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+//import android.R
 
 
 class MainActivity : AppCompatActivity() {
 
-    var i = 0
+    /*val valueOfCoffees = value_of_quantity_text_view.text
+    var numberOfCoffees = valueOfCoffees.toInt()*/
+    var numberOfCoffees = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        value_of_quantity_text_view.text = "$i"
+            //value_of_quantity_text_view.text = "$numberOfCoffees"
 
+    }
+
+    fun increment(view: View) {
+        numberOfCoffees++
+        display(numberOfCoffees)
+
+    }
+
+    fun decrement(view: View) {
+        numberOfCoffees--
+        display(numberOfCoffees)
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     fun submitOrder(view: View) {
-        display(++i)
+        display(numberOfCoffees)
+        displayPrice(numberOfCoffees * 5)
     }
 
     /**
@@ -31,5 +46,15 @@ class MainActivity : AppCompatActivity() {
      */
     private fun display(number: Int) {
         value_of_quantity_text_view.text = "$number"
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private fun displayPrice(number: Int) {
+        //val priceTextView = value_of_price_text_view as TextView
+        //priceTextView.setText(NumberFormat.getCurrencyInstance().format(number))
+        //priceTextView.text = "$ $number"
+        value_of_price_text_view.text = "$ $number"
     }
 }
